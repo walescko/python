28e8ff669f392ffa9dccc0ad508fe92193487893
walescko <walescko@gmail.com>
2022-04-25 14:29:47 -0300

    py - sum of number

---
+++ Livro00/Cap02/prog02.05.py
@@ -0,0 +1,7 @@
+#Programa 2.5 - calcular a soma de três variaveis e apresentar a soma delas
+a = 1
+b = 3
+c = 5
+soma = a + b + c
+print("a =", a, ", b =", b, " e c =",c)
+print("a + b + c =", soma)
