edd1b980f63a8cb2a5c6f7ae641997e44b037d2b
walescko <walescko@gmail.com>
2022-05-01 23:40:38 -0300

    py - true and false

---
+++ Livro00/Cap03/exec03.04.py
@@ -0,0 +1,3 @@
+salario = 1199
+resultado = salario >= 1200
+print("Pagar salario: ", resultado)
---
+++ Livro00/Cap03/exec03.06.py
@@ -0,0 +1,6 @@
+materia1 = 7.0
+materia2 = 6.0
+materia3 = 8.0
+soma = materia1 + materia2 + materia3
+resultado = soma >= 7
+print("Aprovado?", resultado)
