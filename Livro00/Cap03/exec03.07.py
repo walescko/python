3898149188e8ea20fa1d64fbc7f64062d4f92353
walescko <walescko@gmail.com>
2022-05-02 22:40:10 -0300

    py - exercices 7 e 8

---
+++ Livro00/Cap03/exec03.07.py
@@ -0,0 +1,7 @@
+#Exercicio 3.7 - Soma de dois numero inteiros informados pelo usuario
+print("Walescko Software")
+number01 = int(input("Informe o primeiro valor inteiro: "))
+number02 = int(input("Informe o segundo valor inteiro: "))
+soma = number01 + number02
+print(f"A soma de {number01} e {number02} é igual a {soma}.")
+print(f"{number01} + {number02} = {soma}")
---
+++ Livro00/Cap03/exec03.08.py
@@ -0,0 +1,10 @@
+#Exercício 3.8 - Conversão de metros para milimetros
+print("Walescko Software")
+
+emMetros = float(input("Entre com o valor em metros: "))
+emMilimetros = emMetros * 1000
+
+print(f"{emMetros:.2f} m = {emMilimetros:.2f} mm")
+
+
+
