c97c6a3268622a09e6a3e94909dc7aa1f9ad739f
walescko <walescko@gmail.com>
2022-05-06 06:38:07 -0300

    py - exec 12 a 14

---
+++ Livro00/Cap03/exec03.12.py
@@ -0,0 +1,8 @@
+#Calculo do tempo de viagem
+print("Walescko Software")
+distancia = float(input("Qual a distância a ser percorrida em quilometro? "))
+velocidade = float(input("Qual a velocide média em km/h? "))
+
+tempo = distancia / velocidade
+
+print(f"O tempo previsto de viagem é {tempo:.2f} h")
---
+++ Livro00/Cap03/exec03.13.py
@@ -0,0 +1,7 @@
+#Conversor de Celsius para Fahrenheit
+print("Walescko Software")
+celsius = float(input("Entre com a temperatura em Celsius: "))
+
+fahrenheit = ((9*celsius)/5)+32
+
+print(f"A temperatura de {celsius:.2f}ºC equivale a {fahrenheit:.2f}ºC")
