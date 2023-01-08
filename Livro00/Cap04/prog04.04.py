ccfdeb79d4e2c55363f233a3b438c7f49d249a9c
Walescko <walescko@gmail.com>
2023-01-08 20:17:55 -0300

    py - if/else

---
+++ Livro00/Cap04/prog04.04.py
@@ -0,0 +1,6 @@
+#Programa 4.4 - Carro novo ou velho, dependendo da idade com else
+age = int(input("Digite a idade do seu carro: "))
+if age <= 3:
+    print("Seu carro é novo")
+else:
+    print("Seu carro é velho")
