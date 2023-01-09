40f54612363b73cbb6e638038c55dfc55a356750
Walescko <walescko@gmail.com>
2023-01-08 23:35:20 -0300

    py - exercise 6 if/else

---
+++ Livro00/Cap04/exerc04.06.py
@@ -0,0 +1,11 @@
+#Exercicio 4.6 - Escreva um programa que pergunte a distância que um passageiro  deseja percorrer em quilometros. Calcule o preço da viagem, cobrando R$ 0.50 porkm para viagens de até 200km, e R$ 0,45 para viagens mais longas
+print("Walescko Software")
+
+distance = int(input("Qual a distância a ser percorrida? "))
+
+if distance <= 200:
+    price = 0.50
+if distance > 200:
+    price = 0.45
+
+print(f"O valor a ser pago para percorrer a distâncias de {distance} é R$ {distance*price:6.2f}")
