c5f2da442f4c628f5c4342503c286d3566042400
Walescko <walescko@gmail.com>
2023-01-15 21:23:33 -0300

    py - exercise 4.10

---
+++ Livro00/Cap04/exerc04.10.py
@@ -0,0 +1,40 @@
+# Escreva um programa que calcule o preço a pagar pelo fornecimento de energia elétrica.
+# Pergunte a quantidade de kWh consumida e o tipo de instalação: R - Residências, I - Industrias e C - Comércios.
+# Calcule o preço a pagar.
+print("Walescko Loma Linda")
+print("Consumo de Energia Elétrica")
+consumption = int(input("Consumo de kWh/mês? "))
+print("Tipo de Imóvel")
+print("Residencial ..... R")
+print("Industrial ...... I")
+print("Comercial ....... C")
+typeImmobile = input("Qual o tipo de imóvel? ")
+
+amountToPay = 0
+immobile = ""
+
+price = 0.40
+residencial02 = 0.65
+noResidencialPrice01 = 0.55
+noResidencialPrice02 = 0.60
+
+if typeImmobile == "R" or typeImmobile == "r":
+    immobile = "residencial"
+    if consumption > 500:
+        price = residencial02
+elif typeImmobile == "C" or typeImmobile == "c":
+    immobile = "comercial"
+    if consumption <= 1000:
+        price = noResidencialPrice01
+    else:
+        price = noResidencialPrice02
+elif typeImmobile == "I" or typeImmobile == "i":
+    immobile = "industrial"
+    if consumption <= 5000:
+        price = noResidencialPrice01
+    else:
+        price = noResidencialPrice02
+
+amountToPay = consumption * price;
+
+print(f"O valor para  imóvel {immobile} de consumo energia elétrica é  R$ {amountToPay:6.2f}")
