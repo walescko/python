d73961e52f9d4a6093963d2d5a9b61d3329903f6
Walescko <walescko@gmail.com>
2023-01-29 16:50:22 -0300

    py - list

---
+++ Livro00/Cap06/prog06.A.py
@@ -0,0 +1,21 @@
+Z = []
+Z = [15, 8, 9]
+
+print(Z[0])
+length = len(Z)
+
+print(f"Comprimento da lista é {length}")
+
+i = 0
+while i < length:
+    print(f"Elemento {i} é {Z[i]}")
+    i += 1
+i = 0
+
+print(Z[1])
+Z[1] = 3
+print(Z[1])
+
+while i < length:
+    print(f"Elemento {i} é {Z[i]}")
+    i += 1
