65e239c9309bfb9734c3ccedcfd99da95c222b39
Walescko <walescko@gmail.com>
2023-05-03 06:22:19 -0300

    py - database

---
+++ Livro00/Cap06/prog06.07.py
@@ -0,0 +1,23 @@
+#Programa 6.7 - Simulação de uma fila de Banco
+last = 10
+line = list(range(1, last+1))
+while True:
+    print(f"\nExistem {len(line)} clientes na fila")
+    print(f"Fina atual: {line}")
+    print("Digite F para adicionar um cliente ao fim da fila,")
+    print("ou A para realizar o atendimento. S para sair.")
+    operator = input("Operação (F, A ou S:) ")
+    if operator == "A":
+        if len(line) > 0:
+             atendimento = line.pop(0)
+             print((f"Cliente {atendimento} atendido"))
+        else:
+             print("Fila Vazia!Não há ninguém para atender!")
+    elif operator == "F":
+        last += 1
+        line.append(last)
+    elif operator == "S":
+        break
+    else:
+        print("Operação inválida! Digite F, A ou S")
+
