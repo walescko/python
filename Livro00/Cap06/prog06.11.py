2af6146ef946fb83de566da255cfde86b27f7985
walescko <walescko@gmail.com>
2023-07-17 23:13:53 -0300

    py - list

---
+++ Livro00/Cap06/prog06.11.py
@@ -0,0 +1,7 @@
+#Programa 6.11 - Verificação do maior Valor
+L = [1, 7, 2, 4]
+max = L[0]
+for e in L:
+    if e > max:
+        max = e
+print(max)
---
+++ Livro00/Cap06/prog06.13.py
@@ -0,0 +1,11 @@
+#Programa 6.13 - Copy elements for other list
+V = [9, 8, 7, 12, 0, 13, 21]
+P = []
+O = []
+for e in V:
+    if e % 2 == 0:
+        P.append(e)
+    else:
+        O.append(e)
+print('Pares: ', P)
+print('Ímpares: ', O)
