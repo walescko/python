ee23f182954f11a089aeeb866d61a5b594452204
walescko <walescko@gmail.com>
2023-07-25 06:26:30 -0300

    py list with string, int, float

---
+++ Livro00/Cap06/prog06.16.py
@@ -0,0 +1,13 @@
+# Programa 6.16/6.17 - Listas com elementos de tipos diferentes
+
+product01 = ["maçã", 10, 0.30]
+product02 = ["kiwi", 5, 0.75]
+product03 = ["pêra", 4, 0.98]
+
+print(product01)
+print(product02)
+print(product03)
+
+shopping = [product01, product02, product03]
+
+print(shopping)
