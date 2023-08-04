ea6e3eaa6972334f97c6ca9d5937db7da456eba3
walescko <walescko@gmail.com>
2023-08-04 05:29:16 -0300

    py - startswith and endswith

--- Livro00/Cap07/listTest.py
+++ Livro00/Cap07/listTest.py
@@ -6,3 +6,9 @@ L[0] = "a"
 print(L)
 s = "".join(L)
 print(s)
+
+name = "João da Silva"
+print(f"string = {name}")
+print("find João:", name.startswith("João"))
+print("find Juão:", name.startswith("Juão"))
+print("find Silva: ", name.endswith("Silva"))
