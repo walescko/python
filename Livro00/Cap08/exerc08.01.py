0e56d2f2aeac2ec190fa6e7645788cddbe96a4fb
walescko <walescko@gmail.com>
2023-08-11 23:03:43 -0300

    py - max function

---
+++ Livro00/Cap08/exerc08.01.py
@@ -0,0 +1,12 @@
+def max(x, y):
+    if x == y:
+        return "Numeros iguais"
+    elif x > y:
+        return x
+    else:
+        return y
+
+
+print(max(5, 6))
+print(max(2, 1))
+print(max(7, 7))
