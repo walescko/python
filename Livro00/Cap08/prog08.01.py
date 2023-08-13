0f2746e7ae7648f0eeb84273621f5560516473aa
walescko <walescko@gmail.com>
2023-08-13 20:22:05 -0300

    py - functions

--- Livro00/Cap08/prog08.01.py
+++ Livro00/Cap08/prog08.01.py
@@ -6,6 +6,26 @@ def search(list, value):
             return value
     return None
 
+#Boa prática de programação - código limpo: uma função deve fazer apenas uma coisa.
+def sum(L):
+    total = 0
+    for e in L:
+        total += e
+    return total
+def average(L):
+    return sum(L)/len(L)
+
+#Quando se usa uma vez apenas
+def average2(L):
+    total = 0
+    for e in L:
+        total += e
+    return total/len(L)
+
+
 L = [10, 20, 25, 30]
 print(search(L, 25))
 print(search(L, 27))
+print(sum(L))
+print(average(L))
+print(average2(L))
