915ea0ecc2abb5e4f957433f80dfb84a30874e75
walescko <walescko@gmail.com>
2023-08-29 23:25:37 -0300

    py - validation

---
+++ Livro00/Cap08/prog08.09.py
@@ -0,0 +1,8 @@
+# Programa 8.9 - Validação de inteiro usando função
+def faixa_int(question, min, max):
+    while True:
+        v = int(input(question))
+        if v < min or v > max:
+            print(f"Valor inválido. Digite um valor entre {min} e {max}")
+        else:
+            return v
