b5e96a25c15c2941de9baaaa7c49ca4fa42fc9fe
walescko <walescko@gmail.com>
2023-11-30 04:29:21 -0300

    py - lambda

---
+++ Livro00/Cap08/prog08.16.py
@@ -0,0 +1,3 @@
+#Programa 8.16 - Função laqmbda que recebe um valor e retornao dobro dele
+a = lambda x: x * 2
+print(a(3))
---
+++ Livro00/Cap08/prog08.17.py
@@ -0,0 +1,3 @@
+#Programa 8.17 - função lambda que recebe mais de um parâmetro
+increase = lambda a, b: (a * b / 100)
+print(increase(100, 5))
