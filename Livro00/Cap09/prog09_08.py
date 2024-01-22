285fa952b678626fc9d0af4489013227a9fd1e61
walescko <walescko@gmail.com>
2024-01-22 04:01:21 -0300

    py - create html 2

--- Livro00/Cap09/prog09_08.py
+++ Livro00/Cap09/prog09_08.py
@@ -19,7 +19,7 @@ with open("filmes.html", "w", encoding="utf-8") as page:
     for c, v in movies.items():
         page.write(f"<h1>{c}</h1>\n")
         for e in v:
-            page.write(f"<h2>{e}</h2>\n")
+            # page.write(f"<h2>{e}</h2>\n")
+            page.write(f"<p>{e}</p>\n")
 
     page.write("<body></html>")
-    
