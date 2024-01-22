fc89d3a7bed50a08f5d08cc587d15a83274d7fe6
walescko <walescko@gmail.com>
2024-01-22 04:21:23 -0300

    py - check file/directory

---
+++ Livro00/Cap09/directores/prog09_09.py
@@ -0,0 +1,6 @@
+#Programa 9.9 - Verificação se um diretório ou arquivo existe
+import os.path
+if os.path.exists("z"):
+    print("O diretório/pasta existe.")
+else:
+    print("O diretório/pasta não existe.")
