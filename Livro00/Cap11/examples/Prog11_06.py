b4883e368743a3e7c54c2f726e15e3dca732f53b
walescko <walescko@gmail.com>
2024-02-23 17:56:08 -0300

    py - updade commit rollback

--- Livro00/Cap11/examples/Prog11_06.py
+++ Livro00/Cap11/examples/Prog11_06.py
@@ -1,1 +1,17 @@
 #Programa 11.6 - Exemplo de update sem where e com rowcount
+import sqlite3
+from contextlib import closing
+
+with sqlite3.connect("diaryTelephone.db") as connection:
+    with closing(connection.cursor()) as cursor:
+        cursor.execute("""updade agenda
+                        set telephone = '2344-1234'
+                    """)
+        print("Registros alterados: ", cursor.rowcount)
+        if cursor.rowcount == 1:
+            connection.commit()
+            print("Alterações realizada com sucesso!")
+        else:
+            connection.rollback()
+            print("Alteções não foram realizadas")
+
