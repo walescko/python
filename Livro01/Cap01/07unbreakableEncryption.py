bfe869ee53cf05a644860af6f90d5fc1936b5c62
walescko <walescko@gmail.com>
2024-06-16 22:13:36 -0300

    py - pi

--- Livro01/Cap01/07unbreakableEncryption.py
+++ Livro01/Cap01/07unbreakableEncryption.py
@@ -4,3 +4,20 @@ from typing import Tuple
 def random_key(length: int) -> bytes:
     tb: bytes = token_bytes(length)
     return int.from_bytes(tb, 'big')
+
+def encrypt(original: str) -> Tuple[int, int]:
+    original_bytes: bytes = original.encode()
+    dummy: int = random_key(len(original_bytes))
+    original_key: int = int.from_bytes(original_bytes, 'big')
+    encrypt: int = original_key ^ dummy
+    return dummy, encrypt
+
+def decrypt(key1: int, key2: int) -> str:
+    decrypted: int = key1 ^ key2
+    temp: bytes = decrypted.to_bytes((decrypted.bit_length()+7) // 8, 'big')
+    return temp.decode()
+
+if __name__ == '__main__':
+    key1, key2 = encrypt("One Time Pad!")
+    result: str = decrypt(key1, key2)
+    print(result)
