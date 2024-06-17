1d34d3c3e71179e79c9e3768a15a1c7bbccfbcb8
walescko <walescko@gmail.com>
2024-06-17 06:31:23 -0300

    py - hanoi complete

--- Livro01/Cap01/09hanoi.py
+++ Livro01/Cap01/09hanoi.py
@@ -19,4 +19,19 @@ tower_a: Stack[int] = Stack()
 tower_b: Stack[int] = Stack()
 tower_c: Stack[int] = Stack()
 for i in range(1, num_discs +1):
-    tower_a.push(1)
+    tower_a.push(i)
+
+def hanoi(begin: Stack[int], end: Stack[int], temp: Stack[int], n: int) -> None:
+    if n ==1:
+        end.push(begin.pop())
+    else:
+        hanoi(begin, temp, end, n-1)
+        hanoi(begin, end, temp, 1)
+        hanoi(temp, end, begin, n-1)
+
+if __name__ == '__main__':
+    hanoi(tower_a, tower_c, tower_b, num_discs)
+    print(tower_a)
+    print(tower_b)
+    print(tower_c)
+    
