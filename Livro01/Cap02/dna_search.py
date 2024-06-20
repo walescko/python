6d9fb1678b09f21a81d3debc0e41b0d6b7a2a8d7
walescko <walescko@gmail.com>
2024-06-20 04:58:33 -0300

    py - search

--- Livro01/Cap02/dna_search.py
+++ Livro01/Cap02/dna_search.py
@@ -3,3 +3,17 @@ from typing import Tuple, List
 
 Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
+Gene = List[Codon]
+
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+def string_to_gene(s:str) ->Gene:
+    gene: Gene =[]
+    for i in range(0, len(s), 3):
+        if (i+2) >= len(s):
+            return gene
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i+1]], Nucleotide[s[i+2]])
+        gene.append(codon)
+    return gene
+        
