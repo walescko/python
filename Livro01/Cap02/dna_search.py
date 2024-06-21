99949b935ca49e06446a8228e625061a3a2bf5b5
walescko <walescko@gmail.com>
2024-06-21 04:56:28 -0300

    py - search linear

--- Livro01/Cap02/dna_search.py
+++ Livro01/Cap02/dna_search.py
@@ -16,4 +16,20 @@ def string_to_gene(s:str) ->Gene:
         codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i+1]], Nucleotide[s[i+2]])
         gene.append(codon)
     return gene
+
+my_gene: Gene = string_to_gene(gene_str)
+
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
+print(linear_contains(my_gene, acg))
+print(linear_contains(my_gene, gat))
+
+
+
         
