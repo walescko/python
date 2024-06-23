14e289bdf8edc735a7dbdfef1890e0c6ae722ccf
walescko <walescko@gmail.com>
2024-06-23 16:03:04 -0300

    py - dna search

--- Livro01/Cap02/dna_search.py
+++ Livro01/Cap02/dna_search.py
@@ -74,5 +74,36 @@ def linear_contains(iterable: Iterable[T], key: T) -> bool:
 C = TypeVar("C", bound="Comparable")
 
 class Comparable(Protocol):
-    def
+    def __eq_(self, other: Any): -> bool:
+
+    def __lt__(self, other: C) -> bool:
+
+    def __gt__(self: C, other:C) -> bool:
+        return (not self < other) and self != other
+
+    def __le__(self: C, other: C) -> bool:
+        return self < other or self == other
+
+    def __ge__(self: C, other: C) -> bool:
+        return not self < other
+
+    def binary_contains(sequence: Sequence[C], key: C) -> bool:
+        low: int = 0
+        high: int = len(sequence) - 1
+        while low <= high:
+            mid: int = (low + high)//2
+            if sequence[mid] < key:
+                low = mid + 1
+            elif sequence[mid] > key:
+                high = mid - 1
+            else:
+                return True
+        return False
+
+if __name__ == "__main__":
+    print(linear_contain([1, 5, 15, 15, 15, 20], 5))
+    print(binary_contains(["a", "d", "e", "f", "z"], "f"))
+    print(binary_contains(["jonh", "mark", "ronald", "sarah"], "sheila"))
+
+
 
