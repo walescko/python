58eb54b9a11772f515aa889aec688ffe1b5c412e
walescko <walescko@gmail.com>
2024-06-30 18:41:33 -0300

    py maze labirint intro

---
+++ Livro01/Cap02/maze.py
@@ -0,0 +1,17 @@
+from enum import Enum
+from typing import List, NamedTuple, Callable, Optional
+import random
+from math import sqrt
+from generic_search import dfs, bfs, node_to_path, astar, Node
+
+class Cell(str, Enum):
+    EMPTY = " "
+    BLOCKED = "X"
+    START = "S"
+    GOAL = "G"
+    PATH = "*"
+
+class MazeLocation(NamedTuple):
+    row: int
+    column: List
+
