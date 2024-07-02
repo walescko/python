b5c099e4c4e22db07bdb4db8e8fa6f8f6b827882
walescko <walescko@gmail.com>
2024-07-02 04:44:38 -0300

    fix error

--- Livro01/Cap02/dna_search.py
+++ Livro01/Cap02/dna_search.py
@@ -36,7 +36,7 @@ def linear_contains(gene: Gene, key_codon: Codon) -> bool:
             return True
     return False
 
-acg: Codon = (Nucleotide.A, Nucleotide.B, Nucleotide.C)
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
 gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
 print(linear_contains(my_gene, acg))
 print(linear_contains(my_gene, gat))
@@ -48,7 +48,7 @@ def binary_contains(gene: Gene, key_codon: Codon) -> bool:
     while low <= high:
         mid: int = (low + high) // 2
         if gene[mid] < key_codon:
-            low = mind + 1
+            low = mid + 1
         elif gene[mid] > key_codon:
             high = mid -1
         else:
@@ -58,52 +58,6 @@ def binary_contains(gene: Gene, key_codon: Codon) -> bool:
 my_sorted_gene: Gene = sorted(my_gene)
 print(binary_contains(my_sorted_gene, acg))
 print(binary_contains(my_sorted_gene, gat))
-
-from __future__ import annotations
-from typing import TypeVar, Iterable, Sequence, Generic, List, Callble, Set, Deque, Dict, Any, Optional
-from typing_extensions import Protocol
-from heapq import heappush, heappop
-T = TypeVar('T')
-
-def linear_contains(iterable: Iterable[T], key: T) -> bool:
-    for item in iterable:
-        if item == key:
-            return True
-    return False
-
-C = TypeVar("C", bound="Comparable")
-
-class Comparable(Protocol):
-    def __eq_(self, other: Any): -> bool:
-
-    def __lt__(self, other: C) -> bool:
-
-    def __gt__(self: C, other:C) -> bool:
-        return (not self < other) and self != other
-
-    def __le__(self: C, other: C) -> bool:
-        return self < other or self == other
-
-    def __ge__(self: C, other: C) -> bool:
-        return not self < other
-
-    def binary_contains(sequence: Sequence[C], key: C) -> bool:
-        low: int = 0
-        high: int = len(sequence) - 1
-        while low <= high:
-            mid: int = (low + high)//2
-            if sequence[mid] < key:
-                low = mid + 1
-            elif sequence[mid] > key:
-                high = mid - 1
-            else:
-                return True
-        return False
-
-if __name__ == "__main__":
-    print(linear_contain([1, 5, 15, 15, 15, 20], 5))
-    print(binary_contains(["a", "d", "e", "f", "z"], "f"))
-    print(binary_contains(["jonh", "mark", "ronald", "sarah"], "sheila"))
 
 
 
